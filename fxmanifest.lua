801cc04d83221140e1ecb0fa8482624c58b01bcd
Gubner <tom.standley@gmail.com>
2023-03-06 13:33:05 -0800

    Initial

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/gg_prop_frisbee.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gg_fg_teepad.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gg_fg_target.ytyp'
